bb0a293efe5ccec3834d33c728521a8c9fef31df
lvalentine6 <ysr1227@naver.com>
2022-11-02 23:23:28 +0900

    feat : 알고리즘 - 6번

--- src/main/java/mission2/Ascii_6.java
+++ src/main/java/mission2/Ascii_6.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Ascii_5 {
+public class Ascii_6 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char str = br.readLine().charAt(0);
