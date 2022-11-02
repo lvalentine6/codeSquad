572320819480fc95b902a9f6e3e365623d0fae80
lvalentine6 <ysr1227@naver.com>
2022-11-02 23:21:29 +0900

    feat : 알고리즘 - 5번

---
+++ src/main/java/mission2/Ascii_5.java
@@ -0,0 +1,13 @@
+package mission2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ascii_5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char str = br.readLine().charAt(0);
+        System.out.println((int)str);
+    }
+}
