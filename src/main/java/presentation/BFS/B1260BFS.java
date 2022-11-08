c6ae3c199476a0c5bb88deaab30e9603a47df25d
lvalentine6 <ysr1227@naver.com>
2022-11-09 04:02:51 +0900

    feats : BFS 발표 준비

--- src/main/java/presentation/BFS/B1260BFS.java
+++ src/main/java/presentation/BFS/B1260BFS.java
@@ -8,11 +8,12 @@ public class B1260BFS {
     public static void main(String[] args) {
         sb = new StringBuilder();
 
-        // 탐색을 시작할 번호 1이지만 인접행렬과 인덱스를 맞추기 위해서 0으로 설정
+        // 탐색을 시작할 번호는 1이지만 인접행렬과 인덱스를 맞추기 위해서 0으로 설정
         int rootNode= 0;
 
         // 1. 인접 행렬 배열
         int[][] graph = {{0, 1, 1, 1}, {1, 0, 0, 1}, {1, 0, 0, 1}, {1, 1, 1, 0}};
+//        int[][] graph = {{0, 0, 0, 0, 0}, {0, 0, 1, 1, 1}, {0, 1, 0, 0, 1}, {0, 1, 0, 0, 1}, {0, 1, 1, 1, 0}};
 
         // 2. 방문 여부 배열
         boolean[] visited = new boolean[graph.length];
@@ -20,6 +21,7 @@ public class B1260BFS {
         // BFS 메서드 호출
         bfs(graph, visited, rootNode);
 
+        // 탐색 결과 출력
         System.out.println(sb);
 
     }
@@ -33,15 +35,14 @@ public class B1260BFS {
 
         while (!queue.isEmpty()) {
             int nowNode = queue.poll();
-            // 실제 결과 보정
+            // 실제 결과 + 1로 출력값 보정
             sb.append(nowNode + 1).append(" ");
-            for (int i = 0; i < graph.length; i++) {
+            for (int i = 0; i < graph[0].length; i++) {
                 if(graph[nowNode][i] == 1 && !visited[i]) {
                     queue.add(i);
                     visited[i] = true;
                 }
             }
         }
-
     }
 }
