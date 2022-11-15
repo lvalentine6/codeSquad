4834175130c5d26a1602d7bd3439bc661f0d717c
lvalentine6 <ysr1227@naver.com>
2022-11-16 03:19:05 +0900

    feat : problem1

---
+++ src/main/java/mission5/problem1/DiamondCustomer.java
@@ -0,0 +1,14 @@
+package mission5.problem1;
+
+public class DiamondCustomer extends Member {
+    final double DISCOUNT_RATE = 0.1;
+    final double POINT_RATE = 0.1;
+    final int PARKING_RATE = 0;
+
+    public DiamondCustomer(String name, int purchaseAmount, int stayTime) {
+        super(name, purchaseAmount, stayTime);
+        this.discount = purchaseAmount - purchaseAmount * DISCOUNT_RATE;
+        this.point = purchaseAmount * POINT_RATE;
+        this.parkingFee = stayTime * PARKING_RATE;
+    }
+}
