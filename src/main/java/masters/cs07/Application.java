f9b5cf5c7e0ff599bf28cacdd114d3bccb126779
lvalentine6 <ysr1227@naver.com>
2023-01-25 20:02:54 +0900

    Feat : cs07 학습 계획 수정

---
+++ src/main/java/masters/cs07/Application.java
@@ -0,0 +1,8 @@
+package masters.cs07;
+
+public class Application {
+    public static void main(String[] args) {
+        String str = "<!DOCTYPE html><HTML lang=\"ko\"><BODY><P>BOOST<IMG SRC=\"codesquad.kr\"></IMG><BR/></P></BODY></HTML>";
+
+    }
+}
