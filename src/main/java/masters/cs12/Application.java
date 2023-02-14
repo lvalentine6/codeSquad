155b199e57c2b50085c13b30771afca5b9a10119
lvalentine6 <ysr1227@naver.com>
2023-02-14 17:29:34 +0900

    feat : cs11 기본 테스트

--- src/main/java/masters/cs12/Application.java
+++ src/main/java/masters/cs12/Application.java
@@ -3,21 +3,23 @@ package masters.cs12;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class Application {
     public static void main(String[] args)
-            throws URISyntaxException, IOException, SAXException, ParserConfigurationException {
-
-        URL url = new URL("https://lucas.codesquad.kr/masters-2023");
+            throws URISyntaxException, IOException, SAXException, ParserConfigurationException, ExecutionException, InterruptedException {
+        URL url = new URL("https://www.github.com");
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
         httpURLConnection.setRequestMethod("GET");
 
@@ -27,6 +29,7 @@ public class Application {
         BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
         String inputLine;
         StringBuilder response = new StringBuilder();
+        System.out.println(httpURLConnection.getHeaderFields());
 
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
@@ -38,9 +41,9 @@ public class Application {
 
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(response.toString());
+        Document document = dBuilder.parse(new InputSource(new StringReader(String.valueOf(response))));
 
-        NodeList nodes = doc.getElementsByTagName("item");
+        NodeList nodes = document.getElementsByTagName("item");
         for (int i = 0; i < nodes.getLength(); i++) {
             System.out.println("Item " + (i + 1) + ": " + nodes.item(i).getTextContent());
         }
