0a2215ece31eff43f378cd09929119f0bd9d77d9
lvalentine6 <ysr1227@naver.com>
2023-02-17 18:13:33 +0900

    docs : cs13 기본 구현

---
+++ src/main/java/masters/cs13/Application.java
@@ -0,0 +1,13 @@
+package masters.cs13;
+
+public class Application {
+    public static void main(String[] args) {
+        String domain = "https://www.naver.com";
+
+        DNS dns = new DNS(domain);
+        String location = dns.lookup();
+
+
+
+    }
+}
---
+++ src/main/java/masters/cs13/DNS.java
@@ -0,0 +1,38 @@
+package masters.cs13;
+
+import java.net.InetAddress;
+import java.net.URL;
+
+public class DNS {
+    private String domain;
+
+    public DNS(String domain) {
+        this.domain = domain;
+    }
+
+    public String lookup() {
+        StringBuilder stringBuilder = new StringBuilder();
+        InetAddress[] inetAddresses = null;
+
+
+        try {
+            URL url = new URL(domain);
+            inetAddresses = InetAddress.getAllByName(url.getHost());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("DNS 서버 통신중");
+        System.out.println();
+        for (InetAddress ia : inetAddresses) {
+            System.out.println("호스트 이름 : " + ia.getHostName());
+            System.out.println("호스트 주소 : " + ia.getHostAddress());
+            System.out.println("전체 : " + ia);
+            System.out.println();
+
+            stringBuilder.append(ia.getHostAddress()).append("\n");
+        }
+        System.out.println("DNS 조회 완료");
+        return stringBuilder.substring(0, stringBuilder.indexOf("\n"));
+    }
+}
