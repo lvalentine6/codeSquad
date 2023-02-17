a3ee1f104cf19d500d1c3d3e0c7e89334576d1a2
lvalentine6 <ysr1227@naver.com>
2023-02-17 18:33:11 +0900

    docs : cs13 채팅 예시

---
+++ src/main/java/masters/cs13/MyClient.java
@@ -0,0 +1,47 @@
+package masters.cs13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MyClient {
+    public static void main(String[] args) {
+        BufferedReader in = null;
+        PrintWriter out = null;
+
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            socket = new Socket("127.0.0.1", 12345);
+            System.out.println(socket);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            while(true) {
+                System.out.print("전송하기>>> ");
+                String outputMessage = scanner.nextLine();
+                out.println(outputMessage);
+                out.flush();
+                if ("quit".equalsIgnoreCase(outputMessage)) break;
+
+                String inputMessage = in.readLine();
+                System.out.println("From Server: " + inputMessage);
+                if ("quit".equalsIgnoreCase(inputMessage)) break;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                scanner.close();
+                if (socket != null) socket.close();
+                System.out.println("서버연결종료");
+            } catch (IOException e) {
+                System.out.println("소켓통신에러");
+            }
+        }
+    }
+}
---
+++ src/main/java/masters/cs13/MyServer.java
@@ -0,0 +1,55 @@
+package masters.cs13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MyServer {
+    public static void main(String[] args) {
+        BufferedReader in = null;
+        PrintWriter out = null;
+
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            serverSocket = new ServerSocket(12345);
+
+            System.out.println("[Server실행] Client연결대기중...");
+            socket = serverSocket.accept();			// 연결대기
+
+            System.out.println("Client 연결됨.");
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            while(true) {
+                String inputMessage = in.readLine();	// 수신데이터 한줄씩 읽기
+                if ("quit".equalsIgnoreCase(inputMessage)) break;
+
+                System.out.println("From Client: " + inputMessage);
+                System.out.print("전송하기>>> ");
+
+                String outputMessage = scanner.nextLine();
+                out.println(outputMessage);
+                out.flush();
+                if ("quit".equalsIgnoreCase(outputMessage)) break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                scanner.close();		// Scanner 닫기
+                socket.close();			// Socket 닫기
+                serverSocket.close();		// ServerSocket 닫기
+                System.out.println("연결종료");
+            } catch (IOException e) {
+                System.out.println("소켓통신에러");
+            }
+        }
+    }
+}
